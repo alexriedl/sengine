0fb98962ec6fc75139400e05f7d867637b779aa3
Alex Riedl <alexanderbriedl@gmail.com>
2017-10-20 19:22:18 -0500

    more export cleanup

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-export { Shader, SimpleShader, SimpleTextureShader, TextureShader } from './src/Model/Shader';
-export { Buffer, CustomBuffer, DynamicBuffer, RectangleBuffer } from './src/Model/Buffer';
-export { Model, SimpleRectangle, SimpleTextureRectangle, SpriteMap } from './src/Model';
-export { mat4, scalar, vec2, vec3, vec4 } from './src/Math';
-export { Color, Random, Register } from './src/Utils';
-export { Renderer, Game } from './src/Game';
-export { Entity } from './src/Entity';
+export * from './src/Entity';
+export * from './src/Game';
+export * from './src/Math';
+export * from './src/Model';
+export * from './src/Model/Buffer';
+export * from './src/Model/Shader';
+export * from './src/Utils';
