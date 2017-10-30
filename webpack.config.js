1b23205375c16d88a30a7a09fca863ae71ceea9b
Alex Riedl <alexanderbriedl@gmail.com>
2017-10-29 20:21:13 -0500

    more cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,6 @@ var webpack = require('webpack');
 var SRC_DIR = path.join(__dirname, 'src');
 var OUT_DIR = path.join(__dirname, 'dist');
 
-var babelSettings = {
-	plugins: [
-		'transform-class-properties',
-		'transform-object-rest-spread',
-		'transform-runtime'
-	]
-};
-
 module.exports = {
 	context: SRC_DIR,
 	entry: { index: './index.ts' },
@@ -24,7 +16,6 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{ test: /\.ts$/, loaders: ['babel-loader?' + JSON.stringify(babelSettings)], exclude: /node_modules[\\/]/ },
 			{ test: /\.ts$/, include: /src/, loader: 'babel-loader' },
 			{ test: /\.ts$/, loader: 'ts-loader', query: { silent: true } }
 		]
