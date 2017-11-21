cbfecaabbcfcf0bf598703146fe3869bd23911f0
Alex Riedl <alex.riedl@directsupply.com>
2017-11-21 14:38:51 -0600

    Basic camera example

---
+++ examples/src/03-camera/index.ts
@@ -0,0 +1,56 @@
+import { Buffer, Camera2D, Color, Entity, Game, Scene, Shader, vec2, vec3 } from 'sengine';
+
+export default class CameraExample extends Game {
+	private camera: Camera2D;
+	private square: Entity;
+
+	protected left: boolean = false;
+	protected right: boolean = false;
+	protected up: boolean = false;
+	protected down: boolean = false;
+
+	public constructor() {
+		super('game-canvas');
+		this.camera = new Camera2D(new vec2(10, 10));
+		this.setScene(new Scene(this.camera));
+
+		new Entity().setShader(new Shader.SimplerShader(Buffer.createSquare(10), Color.BLUE)).setParent(this.scene);
+		this.square = new Entity()
+			.setShader(new Shader.SimplerShader(Buffer.createSquare(1), Color.GREEN))
+			.setParent(this.scene);
+	}
+
+	public onkeydown = (event: KeyboardEvent): boolean => {
+		const stringCode = String.fromCharCode(event.keyCode);
+		switch (stringCode) {
+			case 'A': case '%': this.left = true; return false;
+			case 'D': case "'": this.right = true; return false;
+			case 'S': case '(': this.down = true; return false;
+			case 'W': case '&': this.up = true; return false;
+		}
+	}
+
+	public onkeyup = (event: KeyboardEvent) => {
+		const stringCode = String.fromCharCode(event.keyCode);
+		switch (stringCode) {
+			case 'A': case '%': this.left = false; return false;
+			case 'D': case "'": this.right = false; return false;
+			case 'S': case '(': this.down = false; return false;
+			case 'W': case '&': this.up = false; return false;
+		}
+	}
+
+	protected update(deltaTime: number): void {
+		const speed = 0.01 * deltaTime;
+		const vx = (-this.left * speed) + (+this.right * speed);
+		const vy = (-this.up * speed) + (+this.down * speed);
+		if (vx || vy) this.square.position = this.square.position.addValues(vx, vy, 0);
+
+		this.camera.position = new vec2(...this.square.position.xy);
+	}
+}
+
+const game = new CameraExample();
+document.onkeydown = game.onkeydown;
+document.onkeyup = game.onkeyup;
+game.start();
