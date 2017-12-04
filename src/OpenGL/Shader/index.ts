fe319d05a966497850f29aafdd32f6224679317f
Alex Riedl <alexanderbriedl@gmail.com>
2017-12-04 07:23:47 -0600

    Fix shader name

--- src/OpenGL/Shader/index.ts
+++ src/OpenGL/Shader/index.ts
@@ -1,11 +1,11 @@
 import AmbientLightShader from './AmbientLightShader';
 import Shader from './Shader';
-import SimplerShader from './SimpleShader';
+import SimpleShader from './SimpleShader';
 import TextureShader from './TextureShader';
 
 export {
 	AmbientLightShader,
 	Shader,
-	SimplerShader,
+	SimpleShader,
 	TextureShader,
 };
