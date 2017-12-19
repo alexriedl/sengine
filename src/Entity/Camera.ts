c0bc2bf83dc4eae2b51330571979d01babe3b984
Alex Riedl <alexanderbriedl@gmail.com>
2017-12-18 22:12:10 -0600

    Setting stuff up for render targets (frame buffer)

--- src/Entity/Camera.ts
+++ src/Entity/Camera.ts
@@ -1,10 +1,10 @@
-import { mat4 } from '../Math';
+import { mat4, vec2 } from '../Math';
+import { Texture } from '../OpenGL';
 import { Color } from '../Utils';
 
 export default abstract class Camera {
-	private static LAST_RENDER_TARGET: WebGLFramebuffer;
 	private backgroundColor: Color = Color.BLACK;
-	public renderTarget: WebGLFramebuffer;
+	public renderTarget: RenderTarget;
 
 		// tslint:disable-next-line:no-bitwise
 	public readonly CLEAR_MASK = WebGLRenderingContext.COLOR_BUFFER_BIT | WebGLRenderingContext.DEPTH_BUFFER_BIT;
@@ -36,12 +36,50 @@ export default abstract class Camera {
 	}
 
 	public setRenderTarget(gl: WebGLRenderingContext): this {
-		// TODO: Validate this test (null or undefined [unbind frame buffer takes only null?])
-		if (this.renderTarget !== Camera.LAST_RENDER_TARGET) {
-			gl.bindFramebuffer(WebGLRenderingContext.FRAMEBUFFER, this.renderTarget);
-			Camera.LAST_RENDER_TARGET = this.renderTarget;
-			console.log('rebound render target');
-		}
+		if (this.renderTarget) this.renderTarget.bind(gl);
 		return this;
+	}
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export abstract class RenderTarget {
+	private static LAST_RENDER_TARGET: RenderTarget;
+	public dimensions: vec2;
+	public get aspect(): number { return this.dimensions.x / this.dimensions.y; }
+
+	public constructor(gl: WebGLRenderingContext) {
+		//
+	}
+
+	protected abstract bindToTarget(gl: WebGLRenderingContext): void;
+
+	public bind(gl: WebGLRenderingContext): void {
+		if (this !== RenderTarget.LAST_RENDER_TARGET) {
+			this.bindToTarget(gl);
+			RenderTarget.LAST_RENDER_TARGET = this;
+		}
+	}
+}
+
+// tslint:disable-next-line:max-classes-per-file
+export class FrameBufferRenderTarget extends RenderTarget {
+	public target: WebGLFramebuffer | null;
+
+	public constructor(gl: WebGLRenderingContext, target: Texture) {
+		super(gl);
+
+		const framebuffer = gl.createFramebuffer();
+		gl.bindFramebuffer(WebGLRenderingContext.FRAMEBUFFER, framebuffer);
+		gl.framebufferTexture2D(WebGLRenderingContext.FRAMEBUFFER,
+			WebGLRenderingContext.COLOR_ATTACHMENT0, target.webGLTarget, target.webGLTexture, 0);
+
+		this.target = framebuffer;
+		this.dimensions = new vec2(target.width, target.height);
+
+		gl.bindFramebuffer(WebGLRenderingContext.FRAMEBUFFER, null);
+	}
+
+	protected bindToTarget(gl: WebGLRenderingContext): void {
+		gl.bindFramebuffer(WebGLRenderingContext.FRAMEBUFFER, this.target);
 	}
 }
